5c24e7b76a1b0446f101067c839bfb71b480fa82
saurabh <skaditaya05082000@gmail.com>
2025-01-26 23:44:59 +0530

    commiting for testing 1st

---
+++ src/main/java/com/spring/demo/fpr/practice/git/Application.java
@@ -0,0 +1,15 @@
+package com.spring.demo.fpr.practice.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+		System.out.println("OKOKOOOKOKKKKOKOKOKKOKOKOKKOKOKOKOKOKOKOKOOKOKOKOKOKOKOKOKO");
+	}
+
+}
